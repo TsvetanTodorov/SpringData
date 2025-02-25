e31d67703265e925a718061cc1f7a00fa106db47
Tsvetan <tstodorofff@gmail.com>
2025-02-25 10:51:52 +0200

    Add UserRepository

---
+++ src/main/java/bg/softuni/repostitories/UserRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.repostitories;
+
+import bg.softuni.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
