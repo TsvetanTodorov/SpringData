c273b1b8c3a37c02b02c7b753665a1f41ec200c6
Tsvetan <tstodorofff@gmail.com>
2025-02-25 10:51:15 +0200

    Create new UserService and UserServiceImpl

---
+++ src/main/java/bg/softuni/services/UserService.java
@@ -0,0 +1,8 @@
+package bg.softuni.services;
+
+import bg.softuni.entities.User;
+
+public interface UserService {
+
+    void register(User user);
+}
---
+++ src/main/java/bg/softuni/services/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package bg.softuni.services;
+
+import bg.softuni.entities.User;
+import bg.softuni.repostitories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void register(User user) {
+        userRepository.save(user);
+    }
+}
